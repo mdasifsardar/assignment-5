851929a9f964ddc999f9641f4e5d0bbf56402673
MD ASIF <mdasifsardar84@gmail.com>
2026-09-13 20:51:25 +0600

    Navbar desktop & responsive is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
+// import MainTechnologies from "./components/Technologies/MainTechnologies";
+// import type { ITechnologies } from "./types/TechnologiesType";
+// import Footer from "./components/Footer/Footer";
+// import DevelopmentSTack from "./components/DevelopmentSTack/DevelopmentSTack";
 import Navbar from "./components/Navbar/Navbar";
-import MainTechnologies from "./components/Technologies/MainTechnologies";
-import type { ITechnologies } from "./types/TechnologiesType";
-import Footer from "./components/Footer/Footer";
-import DevelopmentSTack from "./components/DevelopmentSTack/DevelopmentSTack";
 
-const technologiesData = async (): Promise<ITechnologies[]> => {
-  const res = await fetch("/data.json");
-  const data = await res.json();
-  return data;
-};
+// const technologiesData = async (): Promise<ITechnologies[]> => {
+//   const res = await fetch("/data.json");
+//   const data = await res.json();
+//   return data;
+// };
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <DevelopmentSTack />
+      {/* <DevelopmentSTack />
       <Suspense fallback={<p>Loading....</p>}>
         <MainTechnologies technologiesData={technologiesData()} />
       </Suspense>
-      <Footer />
+      <Footer /> */}
     </>
   );
 };
